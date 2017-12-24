a52eb5a1202564016b9a7b3f367ad381407dd37e
Zain <zainpro@gmx.com>
2017-12-24 11:51:11 -0500

    Comment : first commit on Sun 24 Dec 2017 11:51:11 EST

---
+++ install-screenkey-v1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Thanks to E. Dubois for the scripts
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+sudo rm -rf /opt/screenkey
+sudo apt-get install python-gtk2 python-setuptools python-setuptools-git python-distutils-extra -y
+sudo git clone https://github.com/wavexx/screenkey.git /opt/screenkey
+cd /opt/screenkey
+sudo ./setup.py install
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
---
+++ personal/630-install-personal-settings-variety-v1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Thanks to E. Dubois for the scripts
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+[ -d $HOME"/.config/variety" ] || mkdir -p $HOME"/.config/variety"
+[ -d $HOME"/.config/variety/scripts" ] || mkdir -p $HOME"/.config/variety/scripts"
+
+echo "Copy/pasting scripts for feh/variety to work"
+
+cp settings/variety/* ~/.config/variety/
+
+
+
+echo "################################################################"
+echo "#########       variety installed               ################"
+echo "################################################################"
